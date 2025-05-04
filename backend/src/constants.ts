8a9469dee631d7f2ec500a611577a395ae395424
Joseph Marbella <josephmarbella@arizona.edu>
2025-05-04 15:58:44 -0700

    feat: more constants

--- backend/src/constants.ts
+++ backend/src/constants.ts
@@ -4,6 +4,28 @@ dotenv.config();
 
 export const PORT = process.env.PORT ?? 8080;
 export const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+
+if (OPENAI_API_KEY == null) {
+  throw new Error('OPENAI_API_KEY is not set in the environment variables.');
+}
+
 export const OPENAI_API_ENDPOINT = process.env.OPENAI_API_ENDPOINT;
-export const MONGODB_URI = process.env.MONGODB_URI;
+
+if (OPENAI_API_ENDPOINT == null) {
+  throw new Error('OPENAI_API_ENDPOINT is not set in the environment variables.');
+}
+
+export const MONGODB_URI = process.env.MONGODB_URI ?? 'mongodb://localhost:27017/local';
 export const ANILIST_API = 'https://graphql.anilist.co';
+export const JWT_SECRET =
+  process.env.JWT_SECRET ||
+  (() => {
+    console.error(
+      'JWT_SECRET is not set in the environment variables. Using a default value for development purposes only.'
+    );
+    return 'secret-key';
+  })();
+
+export enum Collections {
+  USERS = 'users',
+}
