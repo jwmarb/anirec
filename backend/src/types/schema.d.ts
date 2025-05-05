e1d169fb3283baa91cf7240a0136b3c6b92a5414
joshjackson1494 <joshuajackson1494@gmail.com>
2025-05-04 17:40:44 -0700

    chore: added recommend file

--- backend/src/types/schema.d.ts
+++ backend/src/types/schema.d.ts
@@ -14,3 +14,35 @@ export type JWTPayload = {
   _id: string; // needs to be converted to ObjectId when querying mongodb
   username: string;
 };
+
+export type Media = {
+  season: string;
+  title: {
+    english: string;
+    native: string;
+    romaji: string;
+  };
+  popularity: number;
+  averageScore: number;
+  genres: string[];
+  format: string;
+  description: string;
+  chapters: null;
+  episodes: number;
+  coverImage: {
+    large: string;
+    extraLarge: string;
+  };
+  endDate: {
+    day: number;
+    month: number;
+    year: number;
+  };
+  seasonYear: number;
+  siteUrl: string;
+  status: string;
+  type: string;
+  volumes: null;
+  isAdult: boolean;
+  id: number;
+};
